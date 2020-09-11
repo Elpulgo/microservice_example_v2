b4508047d386864bfb6fedaec4bad79c86a8eeef
Oscar <oscar.f.larsson@gmail.com>
2020-09-11 21:24:07 +0200

    Added dockerfile to Flights.API and changes in docker compose

---
+++ Flights/Flights.API/Controllers/FlightsController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Flights.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FlightsController : ControllerBase
+    {
+        private readonly ILogger<FlightsController> _logger;
+
+        public FlightsController(ILogger<FlightsController> logger)
+        {
+            _logger = logger;
+        }
+    }
+}
--- Flights/Flights.API/Startup.cs
+++ Flights/Flights.API/Startup.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Flights.Core;
+using Flights.Infrastructure;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -10,6 +12,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Shared.Infrastructure;
 
 namespace Flights.API
 {
@@ -22,30 +25,52 @@ namespace Flights.API
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
+            var postgreConnectionString = "Server=db-flights;Port=5432;Database=flights;User Id=flights_user;Password=flights_user_pass;";
+
+
+            services.AddSingleton<PostgreContext>(sp => new PostgreContext(postgreConnectionString, "flights"));
+            services.AddTransient<IFlightRepository, FlightRepository>();
             services.AddControllers();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
 
             app.UseHttpsRedirection();
-
             app.UseRouting();
-
-            app.UseAuthorization();
-
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
             });
+
+
+
+            // TEST
+            var repo = app.ApplicationServices.GetRequiredService<IFlightRepository>();
+            try
+            {
+
+                var result = repo.InsertAsync(new Flight()
+                {
+                    Id = new Shared.Core.TId()
+                    {
+                        Id = Guid.NewGuid()
+                    },
+                    Destination = "Copenhagen",
+                    Origin = "Malmö",
+                    FlightNumber = "SAS123",
+                    Status = FlightStatus.Boarded
+                }).Result;
+
+            }
+            catch (System.Exception ex)
+            {
+
+                Console.WriteLine($"Exception, cant insert flight {ex.Message}");
+            }
         }
     }
 }
