2297181416dfc51185bab9f79fa59b8f96c5da18
Oscar <elpulgo@users.noreply.github.com>
2020-09-11 20:34:24 +0200

    Added shared

---
+++ Shared/Shared.Core/Models/TId.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace Shared.Core
+{
+    public class TId
+    {
+        public Guid Id { get; set; }
+    }
+}
---
+++ Shared/Shared.Infrastructure/Repositories/IRepository.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Shared.Core;
+
+namespace Shared.Infrastructure
+{
+
+    public interface IRepository<T>
+    {
+        Task DeleteAsync(T entity);
+        Task DeleteByIdAsync(TId id);
+        Task<IReadOnlyList<T>> GetAllAsync();
+        Task<T> GetByIdAsync(TId id);
+        Task<T> InsertAsync(T entity);
+        Task<bool> UpdateAsync(T entity);
+    }
+}
