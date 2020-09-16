18fa542f1370a45a2e4ca2ad80c4a0739c577a55
Oscar <elpulgo@users.noreply.github.com>
2020-09-16 21:07:34 +0200

    Added auditlog web api

---
+++ AuditLog.API/Controllers/AuditLogController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Shared.Infrastructure.Data;
+
+namespace AuditLog.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AuditLogController : ControllerBase
+    {
+        private readonly IEventStoreContext m_Context;
+
+        public AuditLogController(IEventStoreContext context)
+        {
+            m_Context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAuditLogForFlights()
+        {
+            return null;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAuditLogForPassengers()
+        {
+            return null;
+        }
+
+    }
+}
---
+++ AuditLog.API/Startup.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Shared.Infrastructure.Data;
+
+namespace AuditLog.API
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var eventstoreStreamName = Configuration["EVENTSTORE_STREAM_NAME"];
+            var eventstoreConnection = Configuration["EVENTSTORE_CONNECTION"];
+
+            services.AddSingleton<IEventStoreContext>(sp => new EventStoreContext(eventstoreConnection, eventstoreStreamName));
+
+            services.AddControllers();
+
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseHttpsRedirection();
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
