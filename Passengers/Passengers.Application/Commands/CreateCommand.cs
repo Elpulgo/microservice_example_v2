a4471ace3197f69f1c2ae96e06a63de49aaa5bcb
Oscar <elpulgo@users.noreply.github.com>
2020-09-19 20:06:06 +0200

    Added base command

--- Passengers/Passengers.Application/Commands/CreateCommand.cs
+++ Passengers/Passengers.Application/Commands/CreateCommand.cs
@@ -22,32 +22,19 @@ namespace Passengers.Application.Commands
     }
 
     public class CreatePassengerHandler
-        : IRequestHandler<CreatePassengerCommand, PassengerCommandResponse>
+        : BasePassengerCommand, IRequestHandler<CreatePassengerCommand, PassengerCommandResponse>
     {
         private readonly IPassengerEventStorePublisher m_EventStorePublisher;
 
         public CreatePassengerHandler(IPassengerEventStorePublisher eventStorePublisher)
+            : base(eventStorePublisher)
         {
             m_EventStorePublisher = eventStorePublisher ?? throw new ArgumentNullException(nameof(eventStorePublisher));
         }
         public async Task<PassengerCommandResponse> Handle(CreatePassengerCommand request, CancellationToken cancellationToken)
         {
             var eventData = new PassengerEventData(request.Map(), EventTypeOperation.Create, "Create passenger");
-            var response = new PassengerCommandResponse();
-
-            try
-            {
-                await m_EventStorePublisher.Publish(eventData);
-                response.Success = true;
-            }
-            catch (Exception exception)
-            {
-                response.Success = false;
-                response.Error = exception.Message;
-                response.StackTrace = exception.StackTrace;
-            }
-
-            return response;
+            return await base.Handle(eventData, cancellationToken);
         }
     }
 }
