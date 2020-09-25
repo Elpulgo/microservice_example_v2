5c91f828e3af249ae481d1294ca66be360630e26
Oscar <oscar.f.larsson@gmail.com>
2020-09-25 12:14:55 +0200

    Added FlightRpcClient to Passengers.Application, to be able to use the RpcClient with the flight contract

---
+++ Passengers/Passengers.Application/RPC/FlightRpcClient.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Threading.Tasks;
+using Shared.Core.RPC;
+using Shared.Infrastructure.RPC;
+using StreamJsonRpc;
+
+namespace Passengers.Application.RPC
+{
+    public class FlightRpcClient : IFlightRpcClient
+    {
+        private readonly RpcClient m_RpcClient;
+
+        public FlightRpcClient(RpcClient rpcClient)
+        {
+            m_RpcClient = rpcClient ?? throw new ArgumentNullException(nameof(rpcClient));
+        }
+
+        public async Task<bool> FlightExistsAsync(Guid flightId)
+        {
+            using var handler = await m_RpcClient.GetMessageHandlerAsync();
+
+            var jsonRpcClient = JsonRpc.Attach<IFlightContract>(handler);
+
+            return await jsonRpcClient.FlightExistsAsync(new FlightExistsRequest() { FlightId = flightId });
+        }
+    }
+}
