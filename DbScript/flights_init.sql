77ea5f28320b798fff82f0ef4e95275954ea684b
Oscar <elpulgo@users.noreply.github.com>
2020-09-28 12:55:36 +0200

    Made id UNIQUE in dbs..

--- DbScript/flights_init.sql
+++ DbScript/flights_init.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS Flights ( 
-id              UUID                         NOT NULL,
+id              UUID                  UNIQUE NOT NULL,
 destination     VARCHAR(255)                 NOT NULL,
 origin          VARCHAR(255)                 NOT NULL,
 status          INT                          NOT NULL,
-flight_number    VARCHAR(255)                 NOT NULL
+flight_number    VARCHAR(255)                NOT NULL
 );
