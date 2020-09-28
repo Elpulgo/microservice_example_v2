fa1d67f060e761a6c79cff761265aa55dfc05694
Oscar <oscar.f.larsson@gmail.com>
2020-09-28 12:32:48 +0200

    Added transaction on update.

--- Shared/Shared.Infrastructure/Repositories/ReadRepository.cs
+++ Shared/Shared.Infrastructure/Repositories/ReadRepository.cs
@@ -35,7 +35,8 @@ namespace Shared.Infrastructure
         {
             EnsureNotNullOrEmpty(id);
             using var connection = m_Context.Instance;
-            var result = await connection.ExecuteAsync($@"SELECT 1 FROM {m_TableName} WHERE id=@Id", new { Id = id });
+            var result = await connection.ExecuteScalarAsync<int>($@"SELECT 1 FROM {m_TableName} WHERE id=@Id", new { Id = id });
+            Console.WriteLine($"Result if exists is: '{result}', id is: '{id}'");
             return result == 1;
         }
 
