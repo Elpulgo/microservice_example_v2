c0a5385bae38ecbf7efea7e702cb1effd447453f
Oscar <oscar.f.larsson@gmail.com>
2020-09-28 12:55:36 +0200

    Made id UNIQUE in dbs..

--- DbScript/passengers_init.sql
+++ DbScript/passengers_init.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS Passengers ( 
-id              UUID                         NOT NULL,
+id              UUID                  UNIQUE NOT NULL,
 flight_id       UUID                         NOT NULL,
 name            VARCHAR(255)                 NOT NULL,
 status          INT                          NOT NULL
