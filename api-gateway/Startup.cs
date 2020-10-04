65ff4bd28f3e5a860ff6cbf647dab658d5c4c372
Oscar <oscar.f.larsson@gmail.com>
2020-10-04 19:52:57 +0200

    Changed order of CORS headers in api-gateway

--- api-gateway/Startup.cs
+++ api-gateway/Startup.cs
@@ -1,15 +1,7 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 
@@ -38,9 +30,9 @@ namespace api_gateway
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
 
+            app.UseCors(CorsPolicyName);
             app.UseOcelot().Wait();
             app.UseRouting();
-            app.UseCors(CorsPolicyName);
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
